7ce12525d9032f09f8d9d3716fd45b96cd2c8134
Andrey Elagin <daddyingrave@gmail.com>
2021-11-04 15:46:08 +0300

    [kitty] add cyrillyc mappings [firefox] init user.js

---
+++ dot_mozilla/private_firefox/private_i423bl8c.default-release/user.js
@@ -0,0 +1,12 @@
+user_pref("intl.accept_languages",				          "en-US, en");
+user_pref("intl.locale.matchOS",				            false);
+user_pref("browser.search.geoSpecificDefaults",			false);
+user_pref("clipboard.autocopy",					            false);
+user_pref("javascript.use_us_english_locale",			  true);
+user_pref("browser.urlbar.placeholderName",         "DuckDuckGo");
+user_pref("browser.urlbar.suggest.searches",        false);
+user_pref("browser.startup.homepage",               "about:home");
+user_pref("browser.search.countryCode",				      "US");
+user_pref("browser.search.region",				          "US");
+user_pref("browser.search.geoip.url",				        "");
+
